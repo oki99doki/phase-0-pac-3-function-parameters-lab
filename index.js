78e8166e2467ac3bd41ff4c538fb755d012c501c
Stefan Klinkert <s.klinkert@gmail.com>
2024-06-02 22:17:30 -0400

    function lab with parameters done

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+
+
+function introductionWithLanguage(name,language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+  
+
+function introductionWithLanguageOptional(name,language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+  
+
+  introduction("Aki");
+
+  introductionWithLanguage("Aki", "JavaScript");
+
+  introductionWithLanguageOptional("Aki", "JavaScript");
